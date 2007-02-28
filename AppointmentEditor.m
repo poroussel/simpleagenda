5003b0f1cef9e63c56f79db1e03aa4647c404412
Philippe Roussel <p.o.roussel@free.fr>
2007-02-28 21:47:34 +0000

    Display appointment duration in hours and minutes

--- AppointmentEditor.m
+++ AppointmentEditor.m
@@ -1,10 +1,49 @@
 /* emacs buffer mode hint -*- objc -*- */
 
+#import <math.h>
 #import <AppKit/AppKit.h>
 #import "AppointmentEditor.h"
 
+@interface HourFormatter : NSFormatter
+
+- (NSString *)stringForObjectValue:(id)anObject;
+- (BOOL)getObjectValue:(id *)anObject forString:(NSString *)string errorDescription:(NSString **)error;
+- (NSAttributedString *)attributedStringForObjectValue:(id)anObject withDefaultAttributes:(NSDictionary *)attributes;
+
+@end
+
+@implementation HourFormatter
+
+- (NSString *)stringForObjectValue:(id)anObject
+{
+  if (![anObject isKindOfClass:[NSNumber class]])
+    return nil;
+  int m = ([anObject floatValue] - [anObject intValue]) * 100;
+  return [NSString stringWithFormat:@"%dh%2d", [anObject intValue], 60 * m / 100];
+}
+
+- (BOOL)getObjectValue:(id *)anObject forString:(NSString *)string errorDescription:(NSString **)error
+{
+  return NO;
+}
+
+- (NSAttributedString *)attributedStringForObjectValue:(id)anObject withDefaultAttributes:(NSDictionary *)attributes
+{
+  return nil;
+}
+
+@end
+
+
+
 @implementation AppointmentEditor
 
+- (void)awakeFromNib
+{
+  HourFormatter *formatter = [[[HourFormatter alloc] init] autorelease];
+
+  [[durationText cell] setFormatter:formatter];
+}
 
 -(BOOL)editAppointment:(Event *)data
 {
