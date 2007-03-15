533bc556713a0d08de45129ac0ca8e6aa2cf1329
Philippe Roussel <p.o.roussel@free.fr>
2007-03-15 10:44:10 +0000

    Fix time formatter to show 6h00 instead of 6h 0

--- HourFormatter.m
+++ HourFormatter.m
@@ -9,7 +9,7 @@
   if (![anObject isKindOfClass:[NSNumber class]])
     return nil;
   int m = ([anObject floatValue] - [anObject intValue]) * 100;
-  return [NSString stringWithFormat:@"%dh%2d", [anObject intValue], 60 * m / 100];
+  return [NSString stringWithFormat:@"%dh%02d", [anObject intValue], 60 * m / 100];
 }
 
 - (BOOL)getObjectValue:(id *)anObject forString:(NSString *)string errorDescription:(NSString **)error
