fa3a2c63f6220782f099ec37d211bbe691420c3b
Philippe Roussel <p.o.roussel@free.fr>
2007-11-02 17:15:41 +0000

    SimpleAgenda 0.29

--- StoreManager.h
+++ StoreManager.h
@@ -13,12 +13,14 @@
 - (void)registerBackend:(Class)type;
 - (NSArray *)registeredBackends;
 - (Class)backendNamed:(NSString *)name;
+
 - (void)addStoreNamed:(NSString *)name;
 - (void)removeStoreNamed:(NSString *)name;
 - (id <AgendaStore>)storeForName:(NSString *)name;
 - (void)setDefaultStore:(NSString *)name;
 - (id <AgendaStore>)defaultStore;
 - (NSEnumerator *)storeEnumerator;
+
 - (void)synchronise;
 - (void)setDelegate:(id)delegate;
 - (id)delegate;
