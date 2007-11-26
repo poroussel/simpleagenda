b9eb1452cb4634148f5348a9977f240d74177409
Philippe Roussel <p.o.roussel@free.fr>
2007-11-26 11:10:36 +0000

    No need to duplicate method definitions

--- HourFormatter.h
+++ HourFormatter.h
@@ -3,12 +3,4 @@
 #import <Foundation/Foundation.h>
 
 @interface HourFormatter : NSFormatter
-
-- (NSString *)stringForObjectValue:(id)anObject;
-- (BOOL)getObjectValue:(id *)anObject
-	     forString:(NSString *)string 
-      errorDescription:(NSString **)error;
-- (NSAttributedString *)attributedStringForObjectValue:(id)anObject 
-				 withDefaultAttributes:(NSDictionary *)attributes;
-
 @end
