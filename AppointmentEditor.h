792c8fc0218db319ebd9fff76a72ee1d790d7e1c
Philippe Roussel <p.o.roussel@free.fr>
2007-12-05 20:50:41 +0000

    For recurrent events, deal with the end date : initial fix. We have to allow permanent events somehow

--- AppointmentEditor.h
+++ AppointmentEditor.h
@@ -12,15 +12,15 @@
   id durationText;
   id repeat;
   id endDate;
-  id endDateStepper;
   id location;
   id store;
   id allDay;
   id ok;
+  Date *startDate;
 }
 
 - (BOOL)editAppointment:(Event *)data withStoreManager:(StoreManager *)sm;
 - (void)validate:(id)sender;
 - (void)cancel:(id)sender;
-
+- (void)selectFrequency:(id)sender;
 @end
--- AppointmentEditor.m
+++ AppointmentEditor.m
@@ -6,15 +6,22 @@
 #import "AgendaStore.h"
 
 @implementation AppointmentEditor
-
 -(id)init
 {
+  HourFormatter *formatter;
+  NSDateFormatter *dateFormatter;
+
   self = [super init];
   if (self) {
     if (![NSBundle loadNibNamed:@"Appointment" owner:self])
       return nil;
-    HourFormatter *formatter = [[[HourFormatter alloc] init] autorelease];
-    [[durationText cell] setFormatter:formatter];
+    /* FIXME : shouldn't be needed but Gorm just won't set it */
+    [duration setContinuous:YES];
+    formatter = [[[HourFormatter alloc] init] autorelease];
+    dateFormatter = [[[NSDateFormatter alloc] initWithDateFormat:[[NSUserDefaults standardUserDefaults] objectForKey:NSShortDateFormatString] allowNaturalLanguage:NO] autorelease];
+    [durationText setFormatter:formatter];
+    [endDate setFormatter:dateFormatter];
+    [endDate setObjectValue:[NSDate date]];
   }
   return self;
 }
@@ -30,6 +37,7 @@
   [duration setFloatValue:[data duration] / 60.0];
   [durationText setFloatValue:[data duration] / 60.0];
   [repeat selectItemAtIndex:[data interval]];
+  [endDate setEnabled:([data interval] != 0)];
   [location setStringValue:[data location]];
   [allDay setState:[data allDay]];
 
@@ -50,7 +58,7 @@
       [store addItemWithTitle:[aStore description]];
   }
   [store selectItemWithTitle:[[data store] description]];
-
+  startDate = [data startDate];
   ret = [NSApp runModalForWindow:window];
   [window close];
   if (ret == NSOKButton) {
@@ -95,4 +103,39 @@
   [NSApp stopModalWithCode: NSCancelButton];
 }
 
+- (void)controlTextDidChange:(NSNotification *)aNotification
+{
+  id end = [endDate objectValue];
+
+  if (end == nil || ![end isKindOfClass:[NSDate class]])
+    [ok setEnabled:NO];
+  else {
+    [ok setEnabled:YES];
+  }
+}
+
+- (void)selectFrequency:(id)sender
+{
+  Date *futur = [startDate copy];
+  int selected = [repeat indexOfSelectedItem];
+
+  [endDate setEnabled:(selected != 0)];
+  switch (selected) {
+  case 1:
+  case 2:
+    /* Daily and weekly : 1 month by default */
+    [futur changeDayBy:[futur numberOfDaysInMonth]];
+    break;
+  case 3:
+    /* Monthly : 1 year */
+    [futur changeYearBy:1];
+    break;
+  case 4:
+    /* Yearly : 10 years */
+    [futur changeYearBy:10];
+    break;
+  }
+  [endDate setObjectValue:[futur calendarDate]];
+  [futur release];
+}
 @end
