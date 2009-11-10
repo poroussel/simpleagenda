7cbbcce1c42729488367052f8c31eacdceb91a32
Philippe Roussel <p.o.roussel@free.fr>
2009-11-10 20:11:13 +0000

    Absolute alarms seem to work now...

--- AlarmManager.m
+++ AlarmManager.m
@@ -19,32 +19,48 @@
 
 - (void)setAlarmsFromElements:(NSArray *)elements
 {
+  NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
   NSEnumerator *enumerator = [elements objectEnumerator];
   NSEnumerator *enumAlarm;
   Element *element;
   SAAlarm *alarm;
   NSTimer *timer;
+  NSDate *date;
 
   while ((element = [enumerator nextObject])) {
-    if ([[element store] displayed] && [element hasAlarms]) {
-      enumAlarm = [[element alarms] objectEnumerator];
-      while ((alarm = [enumAlarm nextObject])) {
-	if ([alarm isAbsoluteTrigger]) {
-	  NSLog(@"absoluteTrigger %@", [[alarm absoluteTrigger] description]);
-	  timer = [[NSTimer alloc] initWithFireDate:[[alarm absoluteTrigger] calendarDate]
-				           interval:0
-				             target:self
-				           selector:@selector(absoluteTimer:)
-              				   userInfo:alarm
-				            repeats:NO];
-	  [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
-	  [timer release];
-	} else {
-	  NSLog(@"relativeTrigger");
-	}
+    if (![[element store] displayed] || ![element hasAlarms])
+      continue;
+    enumAlarm = [[element alarms] objectEnumerator];
+    while ((alarm = [enumAlarm nextObject])) {
+      if ([alarm isAbsoluteTrigger]) {
+	date = [[alarm absoluteTrigger] calendarDate];
+	if ([date timeIntervalSinceNow] < 0)
+	  break;
+	NSLog(@"absoluteTrigger %@", [date description]);
+	timer = [[NSTimer alloc] initWithFireDate:date
+				         interval:0
+				           target:self
+				         selector:@selector(absoluteTimer:)
+				         userInfo:alarm
+				          repeats:NO];
+	if (timer == nil)
+	  NSLog(@"Erreur creation timer");
+	[runLoop addTimer:timer forMode:NSDefaultRunLoopMode];
+	[timer release];
+      } else {
+	NSLog(@"relativeTrigger");
       }
     }
   }
+}
+
+
+- (void)dataChanged:(NSNotification *)not
+{
+  MemoryStore *store = [not object];
+  NSLog(@"AlarmManager dataChanged:");
+  [self setAlarmsFromElements:[store events]];
+  [self setAlarmsFromElements:[store tasks]];
 }
 
 - (id)init
@@ -77,13 +93,5 @@
   if (singleton == nil)
     singleton = [[AlarmManager alloc] init];
   return singleton;
-}
-
-- (void)dataChanged:(NSNotification *)not
-{
-  MemoryStore *store = [not object];
-  
-  [self setAlarmsFromElements:[store events]];
-  [self setAlarmsFromElements:[store tasks]];
 }
 @end
